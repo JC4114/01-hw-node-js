14b508774339c67a9d0973ef77ae93d081dd8adb
Yurii <4114114jc@gmail.com>
2023-01-30 15:48:30 +0200

    update

---
+++ contacts/index.js
@@ -0,0 +1,13 @@
+const getAllContacts = require("./getAllContacts");
+const listContacts = require("./listContacts");
+const getContactById = require("./getContactById");
+const addContact = require("./addContact");
+const removeContactById = require("./removeContactById");
+
+module.exports = {
+  getAllContacts,
+  listContacts,
+  getContactById,
+  addContact,
+  removeContactById,
+};
---
+++ index.js
@@ -0,0 +1,35 @@
+const { Command } = require("commander");
+const contactsOperation = require("./contacts");
+
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  try {
+    switch (action) {
+      case "list":
+        return await contactsOperation.listContacts();
+      case "get":
+        return await contactsOperation.getContactById(id);
+      case "add":
+        return await contactsOperation.addContact(name, email, phone);
+      case "remove":
+        return await contactsOperation.removeContactById(id);
+      default:
+        console.warn("\x1B[31m Unknown action type!");
+    }
+  } catch (error) {
+    throw error;
+  }
+};
+
+invokeAction(argv);
